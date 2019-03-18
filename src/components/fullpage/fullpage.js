c8256b5eae1b6991c45e685e871ccb8e64a54982
Vadym <horbonosvadym@gmail.com>
2019-03-18 10:53:02 +0200

    #4 Edit example of component

--- src/components/fullpage/fullpage.js
+++ src/components/fullpage/fullpage.js
@@ -4,6 +4,4 @@
 // import 'fullpage.js/dist/jquery.fullpage';
 // import 'fullpage.js/dist/jquery.fullpage.extensions.min';
 
-// $('.layout-container').fullpage({
-//   verticalCentered: true,
-// });
+// $('.selector').fullpage({});
